8d301462966601dc7edd19596a90c5a25f8b467c
hanna004 <100059207+hanna004@users.noreply.github.com>
2022-10-16 23:03:53 -0700

    Create Get Bytes, Uint256.sol

---
+++ Avalanche-Mainnet/Get Bytes, Uint256/Get Bytes, Uint256.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+// block-farms.io
+// Discord=https://discord.gg/PgxRVrDUm7
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract getMultiDataTypeTemplate is ChainlinkClient {
+  using Chainlink for Chainlink.Request;
+
+  bytes public data;
+  string public convertedBytes;
+  uint256 public amount;
+
+  bytes32 private externalJobId;
+  uint256 private oraclePayment;
+
+  constructor(
+  ) {
+    setChainlinkToken(0x5947BB275c521040051D82396192181b413227A3);
+    setChainlinkOracle(0x41fC087cd79fC67f6234B721f8A8AdE68d8aeB8E);
+    externalJobId = "293676225a0e49ec9828b2cb593dcf39";
+    oraclePayment = ((15 * LINK_DIVISIBILITY) / 100); // n * 10**18
+  }
+
+
+  function requestAddressAndAmount(
+  )
+    public
+  {
+    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillBytes.selector);
+    req.add("get", "https://API_endpoint_url");
+    req.add("path1", "data,results1");
+    req.add("path2", "data,results2");
+    req.addInt("times", 100);
+    sendOperatorRequest(req, oraclePayment);
+  }
+
+  event RequestFulfilled(bytes32 indexed requestId,bytes indexed data,uint256 amount);
+
+  function fulfillBytes(bytes32 requestId, bytes memory bytesData, uint256 _amount)
+    public
+    recordChainlinkFulfillment(requestId)
+  {
+    emit RequestFulfilled(requestId, bytesData, _amount);
+    data = bytesData;
+    convertedBytes = string(data);
+    amount = _amount;
+  }
+
+}
