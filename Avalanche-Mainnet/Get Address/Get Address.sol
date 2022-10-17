c53b6a198aa89047088fa4922442991811871c7c
hanna004 <100059207+hanna004@users.noreply.github.com>
2022-10-16 22:40:54 -0700

    Create Get Address.sol

---
+++ Avalanche-Mainnet/Get Address/Get Address.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+// block-farms.io
+// Discord=https://discord.gg/PgxRVrDUm7
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract getAddressTemplate is ChainlinkClient {
+  using Chainlink for Chainlink.Request;
+
+  address public addressVariable;
+
+  bytes32 private externalJobId;
+  uint256 private oraclePayment;
+
+  event RequestFulfilled(bytes32 indexed requestId,address indexed addressVariable);
+
+  constructor() {
+    setChainlinkToken(0x5947BB275c521040051D82396192181b413227A3);
+    setChainlinkOracle(0x41fC087cd79fC67f6234B721f8A8AdE68d8aeB8E);
+    externalJobId = "b91fd273ac784d6e96d6e7f5ad6f441c";
+    oraclePayment = ((15 * LINK_DIVISIBILITY) / 100); // n * 10**18
+  }
+
+  function RequestAddress()
+    public
+  {
+    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillAddress.selector);
+    req.add("get", "https://API_endpoint_url");
+    req.add("path1", "data,results");
+    sendOperatorRequest(req, oraclePayment);
+  }
+
+  function fulfillAddress(bytes32 requestId,address _addressVariable)
+    public
+    recordChainlinkFulfillment(requestId)
+  {
+    emit RequestFulfilled(requestId, _addressVariable);
+    addressVariable = _addressVariable;
+  }
+
+}
